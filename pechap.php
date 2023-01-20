7740b014e34f8dc693f3e94b0fdd561d6e685815
ColubisAvis <122784523+ColubisAvis@users.noreply.github.com>
2023-01-20 15:05:58 +0100

    Update pechap.php

--- pechap.php
+++ pechap.php
@@ -1,23 +1,23 @@
 <?php
-if (isset($_POST["email"], $_POST["nick"], $_POST["typ"], $_POST["temat"], $_POST["tresc"])){
-$email = $_POST["email"];
-$nick = $_POST["nick"];
-$typ = $_POST["typ"];
-$temat = $_POST["temat"];
-$tresc = $_POST["tresc"];
+if (isset($_POST['email'], $_POST['nick'], $_POST['typ'], $_POST['temat'], $_POST['tresc'])){
+$email = $_POST['email'];
+$nick = $_POST['nick'];
+$typ = $_POST['typ'];
+$temat = $_POST['temat'];
+$tresc = $_POST['tresc'];
 
 echo "E-mail: $email<br>";
 echo "Nick: $nick<br>";
 echo "Typ: $typ<br>";
 echo "Temat: $temat<br>";
 echo "Treść: $tresc<br>";
-  if (isset($_POST["userfile"]))
+  if (isset($_POST['userfile']))
   {
-    $userfile=$_POST["userfile"];
+    $userfile=$_POST['userfile'];
     echo "$userfile<br>";
 }
 }
 else{
-echo 'Wpisz dane';
+echo "Wpisz dane";
 }
 ?>
